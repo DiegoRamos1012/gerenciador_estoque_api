8cf0104138bbf953a62f19d2db538feaf2160c12
Diego Ramos dos Santos <diegorsantos09@outlook.com>
2025-12-28 18:26:49 -0300

    Initial commit

---
+++ src/test/java/com/diego_ramos/gerenciador_estoque/GerenciadorEstoqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diego_ramos.gerenciador_estoque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorEstoqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
