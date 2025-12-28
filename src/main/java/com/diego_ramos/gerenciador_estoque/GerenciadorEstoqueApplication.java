1b6b98829997f92860836bd1c914b7266543d442
Diego Ramos dos Santos <diegorsantos09@outlook.com>
2025-12-28 20:43:51 -0300

    Setting local configurations for database

--- src/main/java/com/diego_ramos/gerenciador_estoque/GerenciadorEstoqueApplication.java
+++ src/main/java/com/diego_ramos/gerenciador_estoque/GerenciadorEstoqueApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GerenciadorEstoqueApplication {
-
-	public static void main(String[] args) {
+	static void main(String[] args) {
 		SpringApplication.run(GerenciadorEstoqueApplication.class, args);
 	}
-
 }
---
+++ src/main/java/com/diego_ramos/gerenciador_estoque/entity/Product.java
@@ -0,0 +1,8 @@
+package com.diego_ramos.gerenciador_estoque.entity;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Product {
+
+}
