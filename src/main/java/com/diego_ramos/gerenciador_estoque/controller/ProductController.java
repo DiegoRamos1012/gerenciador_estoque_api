217baa88fe0bdc06bdd77c3f1a07a6f17c51dfd5
Diego Ramos dos Santos <diegorsantos09@outlook.com>
2026-01-02 19:42:41 -0300

    ProductController created and configurated

---
+++ src/main/java/com/diego_ramos/gerenciador_estoque/controller/ProductController.java
@@ -0,0 +1,54 @@
+package com.diego_ramos.gerenciador_estoque.controller;
+
+import com.diego_ramos.gerenciador_estoque.dto.ProductCreateDTO;
+import com.diego_ramos.gerenciador_estoque.dto.ProductResponseDTO;
+import com.diego_ramos.gerenciador_estoque.dto.ProductUpdateDTO;
+import com.diego_ramos.gerenciador_estoque.service.ProductService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+import static com.diego_ramos.gerenciador_estoque.utils.Validators.checkDTOExists;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void create(@RequestBody @Valid ProductCreateDTO dto) {
+        checkDTOExists(dto);
+        productService.createProduct(dto);
+    }
+
+    @GetMapping
+    public List<ProductResponseDTO> findAll() {
+        return productService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ProductResponseDTO findById(@PathVariable UUID id) {
+        return productService.findByID(id);
+    }
+
+    @PatchMapping("/{id}")
+    public ProductResponseDTO updateProduct(@PathVariable UUID id, @RequestBody @Valid ProductUpdateDTO dto) {
+        checkDTOExists(dto);
+        return productService.updateProduct(id, dto);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteProduct(@PathVariable UUID id) {
+        productService.deleteProduct(id);
+    }
+
+}
--- src/main/java/com/diego_ramos/gerenciador_estoque/utils/Validators.java
+++ src/main/java/com/diego_ramos/gerenciador_estoque/utils/Validators.java
@@ -3,7 +3,7 @@ package com.diego_ramos.gerenciador_estoque.utils;
 import com.diego_ramos.gerenciador_estoque.exceptions.BusinessException;
 
 public final class Validators {
-    public static void checkDTOexists(Object dto) {
+    public static void checkDTOExists(Object dto) {
         if (dto == null) {
             throw new BusinessException(("DTO ausente"));
         }
