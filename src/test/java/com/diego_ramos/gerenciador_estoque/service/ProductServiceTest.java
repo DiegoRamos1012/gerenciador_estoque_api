0b1ca83485da469bd2f4fe13d68e6afb20eb1f93
Diego Ramos dos Santos <diegorsantos09@outlook.com>
2026-01-11 19:04:44 -0300

    ProductService is now with 100% coverage tests

--- src/test/java/com/diego_ramos/gerenciador_estoque/service/ProductServiceTest.java
+++ src/test/java/com/diego_ramos/gerenciador_estoque/service/ProductServiceTest.java
@@ -5,6 +5,7 @@ import com.diego_ramos.gerenciador_estoque.dto.ProductCreateDTO;
 import com.diego_ramos.gerenciador_estoque.dto.ProductResponseDTO;
 import com.diego_ramos.gerenciador_estoque.dto.ProductUpdateDTO;
 import com.diego_ramos.gerenciador_estoque.enums.ProductStatus;
+import com.diego_ramos.gerenciador_estoque.exceptions.BusinessException;
 import com.diego_ramos.gerenciador_estoque.extension.TestLoggerExtension;
 import com.diego_ramos.gerenciador_estoque.factory.ProductFactoryTest;
 import com.diego_ramos.gerenciador_estoque.repository.ProductRepository;
@@ -17,11 +18,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.math.BigDecimal;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
@@ -52,15 +53,18 @@ public class ProductServiceTest {
         when(productRepository.save(any(Product.class))).thenAnswer(invocation -> invocation.getArgument(0));
         assertDoesNotThrow(() -> productService.createProduct(fakeDto));
         verify(productRepository, times(1)).save(any(Product.class));
-
-
     }
 
     @Test
     void testProductRemoval() {
         UUID productId = UUID.randomUUID();
+        UUID inexistentProduct = UUID.randomUUID();
 
         when(productRepository.existsById(productId)).thenReturn(true);
+        when(productRepository.existsById(inexistentProduct)).thenReturn(false);
+
+        assertThrows(BusinessException.class, () ->
+                productService.deleteProduct(inexistentProduct));
 
         productService.deleteProduct(productId);
 
@@ -98,7 +102,73 @@ public class ProductServiceTest {
 
     @Test
     void testProductList() {
-        // TODO: Inserir teste pra verificar se utiliza o findById e findAll
+        Product fakeProduct1 = ProductFactoryTest.createFakeProduct();
+        Product fakeProduct2 = ProductFactoryTest.createFakeProduct();
 
+        List<Product> fakeProducts = List.of(fakeProduct1, fakeProduct2);
+
+        when(productRepository.findAll()).thenReturn(fakeProducts);
+        List<ProductResponseDTO> products = assertDoesNotThrow(() -> productService.findAll());
+        assertEquals(2, products.size(), "Erro: Deve retornar 2 produtos");
+        assertEquals(fakeProduct1.getName(), products.getFirst().name());
+        assertEquals(fakeProduct2.getName(), products.getFirst().name());
+        verify(productRepository, times(1)).findAll();
+    }
+
+    @Test
+    void shouldThrowBusinessExceptionWhenProductAlreadyExists() {
+        Product fakeProduct = ProductFactoryTest.createFakeProduct();
+
+        ProductCreateDTO fakeDto = new ProductCreateDTO(
+                fakeProduct.getName(),
+                fakeProduct.getPrice(),
+                fakeProduct.getQuantity(),
+                fakeProduct.getDescription(),
+                fakeProduct.getStatus()
+        );
+
+        when(productRepository.existsByNameIgnoreCase(fakeDto.name()))
+                .thenReturn(true);
+
+        assertThrows(BusinessException.class, () ->
+                productService.createProduct(fakeDto));
+
+        verify(productRepository, never()).save(any(Product.class)); // Garantir que o "save" não tenha sido chamado nenhuma vez pra qualquer objeto de Product
+    }
+
+    @Test
+    void shouldThrowBusinessExceptionWhenProductNotExists() {
+        UUID inexistentProduct = UUID.randomUUID();
+
+        when(productRepository.findById(inexistentProduct)).thenReturn(Optional.empty());
+
+        assertThrows(BusinessException.class, () ->
+                productService.findByID(inexistentProduct));
+
+        verify(productRepository, times(1)).findById(inexistentProduct);
+    }
+
+    @Test
+    void shouldKeepOldValuesWhenUpdateDtoHasNullFields() {
+        Product fakeProduct = ProductFactoryTest.createFakeProduct();
+        UUID id = fakeProduct.getId();
+
+        when(productRepository.findById(id)).thenReturn(Optional.of(fakeProduct));
+        when(productRepository.save(any(Product.class))).thenAnswer(invocation -> invocation.getArgument(0));
+
+        ProductUpdateDTO dto = new ProductUpdateDTO(
+                null,
+                null,
+                null,
+                null,
+                null
+        );
+
+        ProductResponseDTO updated = productService.updateProduct(id, dto);
+
+        assertEquals(fakeProduct.getName(), updated.name());
+        assertEquals(fakeProduct.getPrice(), updated.price());
+        assertEquals(fakeProduct.getQuantity(), updated.quantity());
+        assertEquals(fakeProduct.getDescription(), updated.description());
     }
 }
