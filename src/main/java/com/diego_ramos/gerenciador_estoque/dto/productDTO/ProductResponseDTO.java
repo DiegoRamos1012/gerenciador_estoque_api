8e47184e06f1b92307d0062c508eebfd1a3f12b3
DiegoRamos1012 <diegorsantos09@outlook.com>
2026-02-24 21:44:48 -0300

    Removed JsonFormat from lastTimeChanged

--- src/main/java/com/diego_ramos/gerenciador_estoque/dto/productDTO/ProductResponseDTO.java
+++ src/main/java/com/diego_ramos/gerenciador_estoque/dto/productDTO/ProductResponseDTO.java
@@ -2,7 +2,6 @@ package com.diego_ramos.gerenciador_estoque.dto.productDTO;
 
 import com.diego_ramos.gerenciador_estoque.domain.Product;
 import com.diego_ramos.gerenciador_estoque.enums.ProductStatus;
-import com.fasterxml.jackson.annotation.JsonFormat;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -23,7 +22,6 @@ public record ProductResponseDTO(
 
         ProductStatus status,
 
-        @JsonFormat(pattern = "yyyy/MM/dd HH:mm:ss")
         LocalDateTime lastTimeChanged
 ) {
     public static ProductResponseDTO from(Product product) {
